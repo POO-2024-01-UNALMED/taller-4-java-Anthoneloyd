5269b59b4950a0c154c1ff2a8b2d246d33161fcf
Andrés Garcés <105695594+Anthoneloyd@users.noreply.github.com>
2024-02-29 08:59:17 -0500

    Update Tipo.java

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -3,10 +3,4 @@ package classroom;
 public enum Tipo {
 	DISCIPLINAR,FUNDAMENTACION,ELECTIVA;
 	
-	private int codigo;
-	private String nombre;
-	public Tipo(int codigo,String nombre) {
-		this.codigo = codigo;
-		this.nombre = nombre;
-	}
 }
